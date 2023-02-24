66e59921a2fe30c449d5b8663bee61b24ef0839e
luyueyin <12898@coderacademy.edu.au>
2023-02-24 21:45:30 +1100

    Navbar - mobile

--- src/components/NavbarDesktop.jsx
+++ src/components/NavbarDesktop.jsx
@@ -1,14 +1,11 @@
 import { ListItemText } from "@mui/material";
 import { NavList, NavbarContainer, NavbarHeader } from "../styled/Navbar";
-import Divider from '@mui/material/Divider';
 
 export default function NavbarMobile({ matches }) {
 
     return (
         <NavbarContainer>
-            <NavbarHeader>
-                Petpels
-            </NavbarHeader>
+            <NavbarHeader>Petpels</NavbarHeader>
             <NavList type="row">
                 <ListItemText primary="Home" />
                 <ListItemText primary="Pet Parent" />
--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -1,5 +1,15 @@
+import { IconButton } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu'
+import { NavbarContainer, NavbarHeader } from "../styled/Navbar";
 
 
 export default function NavbarDesktop({ matches }) {
-    return <h1>Mobile</h1>
+    return (
+        <NavbarContainer>
+            <NavbarHeader variant="h4">Petpels</NavbarHeader>
+            <IconButton>
+                <MenuIcon />
+            </IconButton>
+        </NavbarContainer>
+    )
 }
