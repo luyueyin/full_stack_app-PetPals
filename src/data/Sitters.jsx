57cfed0fd00730cf3793ae89e6b9b54f0a67de70
luyueyin <12898@coderacademy.edu.au>
2023-03-03 00:50:34 +1100

    Added Fake User Data

--- src/data/Sitters.jsx
+++ src/data/Sitters.jsx
@@ -1,13 +1,14 @@
 export const cards = [
     {
-        id: 1,
+        "id": 1,
         "name": "Alice",
         "experience": "5 years",
         "phone": "(04) 1666-4784",
         "email": "alice@example.com",
         "address": "Catalinaton",
-        "Description:": "Professional pet sitter with 5 years of experience. Flexible scheduling to accommodate clients' needs",
-        "img": "src={src='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShaggyMullet&accessoriesType=Blank&hairColor=Blonde&facialHairType=BeardMedium&facialHairColor=BrownDark&clotheType=CollarSweater&clotheColor=Gray01&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Sad&skinColor=Yellow'}"
+        "description": "Professional pet sitter with 5 years of experience. Flexible scheduling to accommodate clients' needs",
+        "img": "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShaggyMullet&accessoriesType=Blank&hairColor=Blonde&facialHairType=BeardMedium&facialHairColor=BrownDark&clotheType=CollarSweater&clotheColor=Gray01&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Sad&skinColor=Yellow",
+        "value": "5"
     },
     {
         "id": 2,
@@ -16,7 +17,9 @@ export const cards = [
         "phone": "(04) 4635-7736",
         "email": "bob@example.com",
         "address": "East Vinnieton",
-        "Description:": "Excellent communication skills, including regular updates for pet owners while they are away."
+        "description": "Excellent communication skills, including regular updates for pet owners while they are away.",
+        "img": "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortCurly&accessoriesType=Round&hairColor=PastelPink&facialHairType=Blank&facialHairColor=Black&clotheType=GraphicShirt&clotheColor=Blue03&graphicType=Cumbia&eyeType=Wink&eyebrowType=UpDownNatural&mouthType=ScreamOpen&skinColor=Tanned",
+        "value": "4.5"
     },
     {
         "id": 3,
@@ -25,7 +28,9 @@ export const cards = [
         "phone": "(02) 4861-4744",
         "email": "charlie@example.com",
         "address": "South Noemietown",
-        "Description:": "Skilled in providing exceptional care and attention to pets of all types and sizes."
+        "description": "Skilled in providing exceptional care and attention to pets of all types and sizes.",
+        "img": "https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Wayfarers&facialHairType=BeardMajestic&facialHairColor=Platinum&clotheType=Hoodie&clotheColor=Gray01&eyeType=EyeRoll&eyebrowType=FlatNatural&mouthType=Smile&skinColor=Brown",
+        "value": "3.8"
     },
     {
         "id": 4,
@@ -34,7 +39,9 @@ export const cards = [
         "phone": "(02) 0395-4030",
         "email": "david@example.com",
         "address": "Bergstromshire",
-        "Description:": "Knowledgeable about different animal behaviors and specific pet needs."
+        "description": "Knowledgeable about different animal behaviors and specific pet needs.",
+        "img": "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads02&accessoriesType=Prescription01&hairColor=BlondeGolden&facialHairType=BeardLight&facialHairColor=Auburn&clotheType=ShirtScoopNeck&clotheColor=Gray02&eyeType=Hearts&eyebrowType=SadConcernedNatural&mouthType=Sad&skinColor=Brown",
+        "value": "4.8"
     },
     {
         "id": 5,
@@ -43,7 +50,9 @@ export const cards = [
         "phone": "(04) 8647-5795",
         "email": "emily@example.com",
         "address": "Ezekielchester",
-        "Description:": "Experienced in administering medications and providing basic first aid."
+        "description": "Excellent communication skills, including regular updates for pet owners while they are away.",
+        "img": "https://avataaars.io/?avatarStyle=Circle&topType=LongHairCurvy&accessoriesType=Prescription02&hairColor=Blonde&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Happy&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light",
+        "value": "4.3"
     },
     {
         "id": 6,
@@ -52,7 +61,9 @@ export const cards = [
         "phone": "(04) 4892-0265",
         "email": "frank@example.com",
         "address": "Parisianshire",
-        "Description:": "Committed to ensuring pets receive adequate exercise and playtime."
+        "description": "Able to handle emergencies and know when to seek veterinary care. Willing to go above and beyond to ensure pets are happy and well-cared for",
+        "img": "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Blank&hairColor=Black&facialHairType=MoustacheFancy&facialHairColor=BlondeGolden&clotheType=Hoodie&clotheColor=Red&eyeType=Default&eyebrowType=UnibrowNatural&mouthType=Sad&skinColor=Brown",
+        "value": "4.9"
     },
     {
         "id": 7,
@@ -61,7 +72,9 @@ export const cards = [
         "phone": "(02) 4424-0664",
         "email": "grace@example.com",
         "address": "Lake Jaymeton",
-        "Description:": "Able to handle emergencies and know when to seek veterinary care. Willing to go above and beyond to ensure pets are happy and well-cared for"
+        "description": "Able to handle emergencies and know when to seek veterinary care. Willing to go above and beyond to ensure pets are happy and well-cared for",
+        "img": "https://avataaars.io/?avatarStyle=Circle&topType=LongHairDreads&accessoriesType=Wayfarers&hatColor=Heather&hairColor=Brown&facialHairType=MoustacheMagnum&facialHairColor=Blonde&clotheType=BlazerSweater&clotheColor=Blue01&graphicType=SkullOutline&eyeType=Happy&eyebrowType=Default&mouthType=Disbelief&skinColor=Light",
+        "value": "4.3"
     },
     {
         "id": 8,
@@ -70,6 +83,8 @@ export const cards = [
         "phone": "(04) 7835-7028",
         "email": "henry@example.com",
         "address": "North Filibertoside",
-        "Description:": "Trustworthy and dependable, with a proven track record of satisfied clients."
+        "description": "Trustworthy and dependable, with a proven track record of satisfied clients.",
+        "img": "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShaggyMullet&accessoriesType=Kurt&hairColor=PastelPink&facialHairType=BeardLight&facialHairColor=Black&clotheType=ShirtVNeck&clotheColor=PastelGreen&eyeType=Dizzy&eyebrowType=DefaultNatural&mouthType=Serious&skinColor=DarkBrown",
+        "value": "4.1"
     }
 ]
